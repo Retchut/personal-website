210e2cffd58965a68bf9d37f24c380cb304641bf
retchut <retchut@protonmail.ch>
2022-03-10 12:07:22 +0000

    feat: add project data

--- src/Assets/Data/ProjectData.js
+++ src/Assets/Data/ProjectData.js
@@ -1,45 +1,45 @@
 export const ProjectData =
 [
     {
-        "Id" : 1,
-        "Name" : "Proj1",
-        "Description" : "Desc1",
-        "ImgPath" : "Img1",
-        "URL" : "Url1"
+        id : 1,
+        name : "Arch Quick Setup",
+        description: "A script and files to make setting up arch on a fresh install just the way I like like it simple and easy, with just a few simple strokes and key presses. Constant WIP.",
+        imgPath : '../../assets/img/projects/arch-quick-setup.jpg',
+        url : "https://github.com/Retchut/arch-quick-setup"
     },
     {
-        "Id" : 2,
-        "Name" : "Proj2",
-        "Description" : "Desc2",
-        "ImgPath" : "Img2",
-        "URL" : "Url2"
+        id : 2,
+        name : "LCOM 2020/2021",
+        description: "Repository for MIEIC's LCOM class of the year 2021/2022. Contains the lab assignments and my final project project for the class,a clone of the classic Space Invaders.",
+        imgPath : '../../assets/img/projects/LCOM-2020-2021.png',
+        url : "https://github.com/Retchut/LCOM-2020-2021"
     },
     {
-        "Id" : 3,
-        "Name" : "Proj3",
-        "Description" : "Desc3",
-        "ImgPath" : "Img3",
-        "URL" : "Url3"
+        id : 3,
+        name : "NCGM Learning Project",
+        description: "My learning project developed for NCGM. A simple text based adventure about escaping a locked room.",
+        imgPath : '../../assets/img/projects/LearningProject.png',
+        url : "https://github.com/Retchut/LearningProject1920"
     },
     {
-        "Id" : 4,
-        "Name" : "Proj4",
-        "Description" : "Desc4",
-        "ImgPath" : "Img4",
-        "URL" : "Url4"
+        id : 4,
+        name : "SRT Editor",
+        description: "Simple srt file editing tool created to aid me in my manual modification (and occasional creation) of subtitle files.",
+        imgPath : '../../assets/img/projects/placeholder.png',
+        url : "https://github.com/Retchut/srt-editor"
     },
     {
-        "Id" : 5,
-        "Name" : "Proj5",
-        "Description" : "Desc5",
-        "ImgPath" : "Img5",
-        "URL" : "Url5"
+        id : 5,
+        name : "PROG 2019/2020",
+        description: "Repository for MIEIC's PROG class of the year 2021/2022. Contains the projects my group developed for this class: games based on oware and scrabble.",
+        imgPath : '../../assets/img/projects/class-stuff.png',
+        url : "https://github.com/Retchut/Prog-2019-2020"
     },
     {
-        "Id" : 6,
-        "Name" : "Proj6",
-        "Description" : "Desc6",
-        "ImgPath" : "Img6",
-        "URL" : "Url6"
+        id : 6,
+        name : "MNUM 2020/2021",
+        description: "Repository for MIEIC's MNUM class of the year 2021/2022. Contains solutions for a few exams I solved while studying, as well as my group's project for this class.",
+        imgPath : '../../assets/img/projects/class-stuff.png',
+        url : "https://github.com/Retchut/MNUM-2020-2021"
     }
 ];
