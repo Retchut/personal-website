76bcb11e71d878871745acef0e6d4b9ef12128dc
retchut <retchut@protonmail.ch>
2022-03-12 11:13:57 +0000

    fix: fix imports and project data

--- src/Assets/Data/ProjectData.js
+++ src/Assets/Data/ProjectData.js
@@ -3,43 +3,62 @@ export const ProjectData =
     {
         id : 1,
         name : "Arch Quick Setup",
-        description: "A script and files to make setting up arch on a fresh install just the way I like like it simple and easy, with just a few simple strokes and key presses. Constant WIP.",
-        imgPath : '../../assets/img/projects/arch-quick-setup.jpg',
+        description : "A script and files to make setting up arch on a fresh install just the way I like like it simple and easy, with just a few simple strokes and key presses. Constant WIP.",
+        technologies : [
+            "bash"
+        ],
+        imgPath : "../../Assets/Images/placeholder.jpg",
         url : "https://github.com/Retchut/arch-quick-setup"
     },
     {
         id : 2,
         name : "LCOM 2020/2021",
-        description: "Repository for MIEIC's LCOM class of the year 2021/2022. Contains the lab assignments and my final project project for the class,a clone of the classic Space Invaders.",
-        imgPath : '../../assets/img/projects/LCOM-2020-2021.png',
+        description : "Repository for MIEIC's LCOM class of the year 2021/2022. Contains the lab assignments and my final project project for the class,a clone of the classic Space Invaders.",
+        technologies : [
+            "C"
+        ],
+        imgPath : "../../Assets/Images/placeholder.jpg",
         url : "https://github.com/Retchut/LCOM-2020-2021"
     },
     {
         id : 3,
         name : "NCGM Learning Project",
-        description: "My learning project developed for NCGM. A simple text based adventure about escaping a locked room.",
-        imgPath : '../../assets/img/projects/LearningProject.png',
+        description : "My learning project developed for NCGM. A simple text based adventure about escaping a locked room.",
+        imgPath : "../../Assets/Images/placeholder.jpg",
+        technologies : [
+            "Unity",
+            "C#"
+        ],
         url : "https://github.com/Retchut/LearningProject1920"
     },
     {
         id : 4,
         name : "SRT Editor",
-        description: "Simple srt file editing tool created to aid me in my manual modification (and occasional creation) of subtitle files.",
-        imgPath : '../../assets/img/projects/placeholder.png',
+        description : "Simple srt file editing tool created to aid me in my manual modification (and occasional creation) of subtitle files.",
+        imgPath : "../../Assets/Images/placeholder.jpg",
+        technologies : [
+            "C++"
+        ],
         url : "https://github.com/Retchut/srt-editor"
     },
     {
         id : 5,
         name : "PROG 2019/2020",
-        description: "Repository for MIEIC's PROG class of the year 2021/2022. Contains the projects my group developed for this class: games based on oware and scrabble.",
-        imgPath : '../../assets/img/projects/class-stuff.png',
+        description : "Repository for MIEIC's PROG class of the year 2021/2022. Contains the projects my group developed for this class: games based on oware and scrabble.",
+        imgPath : "../../Assets/Images/placeholder.jpg",
+        technologies : [
+            "C++"
+        ],
         url : "https://github.com/Retchut/Prog-2019-2020"
     },
     {
         id : 6,
         name : "MNUM 2020/2021",
-        description: "Repository for MIEIC's MNUM class of the year 2021/2022. Contains solutions for a few exams I solved while studying, as well as my group's project for this class.",
-        imgPath : '../../assets/img/projects/class-stuff.png',
+        description : "Repository for MIEIC's MNUM class of the year 2021/2022. Contains solutions for a few exams I solved while studying, as well as my group's project for this class.",
+        technologies : [
+            "Python"
+        ],
+        imgPath : "../../Assets/Images/placeholder.jpg",
         url : "https://github.com/Retchut/MNUM-2020-2021"
     }
 ];
--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,12 +1,17 @@
-function Project(projectData){
-    return (
-      <div className="background-box m-4">
-        <h1>{projectData.name}</h1>
-        <p>{projectData.description}</p>
-        <img src={projectData.imgPath}></img>
-        <a href={projectData.url}>Git Repo</a>
-      </div>
-    );
+function Project(props){
+  const { projectData } = props;
+  // console.log(projectData.imgPath);
+  return (
+    <div className="background-box m-4 mw-100">
+      <h1>{projectData.name}</h1>
+      <p>{projectData.description}</p>
+      <div className="row">{projectData.technologies.map((technology, index) => <div key={projectData.name + index} className="col"><p>technology</p></div>)}</div>
+      <img src={ require('../../Assets/Images/placeholder.jpg') }></img>
+      {/* <img src={ require(projectData.imgPath) }></img> */}
+      
+      <a href={projectData.url}>Git Repo</a>
+    </div>
+  );
 }
 
 export default Project;
