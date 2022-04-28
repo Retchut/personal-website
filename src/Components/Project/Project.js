005e8b5ce11b606d02797ff2809480b9d60aca07
retchut <retchut@protonmail.ch>
2022-04-28 18:14:12 +0100

    feat: reverse project layout

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,12 +1,12 @@
 function Project(props){
   const { projectData } = props;
   return (
-    <>
+    <div className="mx-3 h-100 background-box rounded p-4 ">
       <div className="row">
-        <div className="col">
-          <img className="w-100 h-100" src={projectData.imgPath}></img>
+        <div className="col-lg-4 d-flex align-items-center">
+          <img className="w-100 h-100 rounded" src={projectData.imgPath}></img>
         </div>
-        <div className="col">
+        <div className="col-lg-8">
           <div className="row">
             <h1>{projectData.name}</h1>
           </div>
@@ -17,7 +17,10 @@ function Project(props){
       </div>
 
       <div className="row">
-        <div className="col">
+        <div className="col-lg-4 link-buttons d-flex justify-content-center">
+          <a href={projectData.url}>Git Repo</a>
+        </div>
+        <div className="col-lg-8">
           <div className="row my-1 h-100">
             {projectData.technologies.map((technology, index) =>
               <div key={projectData.name + index} className="col d-flex justify-content-center align-items-center">
@@ -25,11 +28,8 @@ function Project(props){
               </div>)}
           </div>
         </div>
-        <div className="col link-buttons d-flex justify-content-center">
-          <a href={projectData.url}>Git Repo</a>
-        </div>
       </div>
-    </>
+    </div>
 
   );
 }
--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -36,7 +36,7 @@ function Projects(){
                                 {
                                     projectRow.map(
                                         project =>
-                                        <div key={project.id} className="col mx-3 mb-4 h-100 background-box rounded p-4">
+                                        <div key={project.id} className="col mb-4">
                                             <Project projectData={project} />
                                         </div>
                                     )
