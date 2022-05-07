d394e2fdae4b45170d1ffc133aed3ddc4854251c
retchut <retchut@protonmail.ch>
2022-05-07 13:57:02 +0100

    chore: update project page data

--- src/Assets/Data/ProjectData.js
+++ src/Assets/Data/ProjectData.js
@@ -2,40 +2,43 @@ export const ProjectData =
 [
     {
         id : 1,
-        name : "Arch Quick Setup",
-        description : "A script and files to make setting up arch on a fresh install just the way I like like it simple and easy, with just a few simple strokes and key presses. Constant WIP.",
+        name : "This website",
+        description : "Repository holding this website's code. Pages were made to be independent from the data displayed on them. WIP",
         technologies : [
-            "bash"
+            "React",
+            "Bootstrap 5",
+            "HTML",
+            "CSS",
+            "JavaScript"
         ],
-        imgPath : "./Images/Projects/placeholder.jpg",
-        url : "https://github.com/Retchut/arch-quick-setup"
+        imgPath : "./Images/Projects/ReactLogo.png",
+        url : "https://github.com/Retchut/personal-website"
     },
     {
         id : 2,
+        name : "Arch Quick Setup",
+        description : "Some scripts and files to make setting up arch on a fresh install just the way I like like it simple and easy, with just a few simple strokes and key presses. Constant WIP.",
+        technologies : [
+            "bash"
+        ],
+        imgPath : "./Images/Projects/ArchQuickSetup.jpg",
+        url : "https://github.com/Retchut/arch-quick-setup"
+    },
+    {
+        id : 3,
         name : "LCOM 2020/2021",
         description : "Repository for MIEIC's LCOM class of the year 2021/2022. Contains the lab assignments and my final project project for the class,a clone of the classic Space Invaders.",
         technologies : [
             "C"
         ],
-        imgPath : "./Images/Projects/placeholder.jpg",
+        imgPath : "./Images/Projects/LCOM-2020-2021.png",
         url : "https://github.com/Retchut/LCOM-2020-2021"
-    },
-    {
-        id : 3,
-        name : "NCGM Learning Project",
-        description : "My learning project developed for NCGM. A simple text based adventure about escaping a locked room.",
-        imgPath : "./Images/Projects/placeholder.jpg",
-        technologies : [
-            "Unity",
-            "C#"
-        ],
-        url : "https://github.com/Retchut/LearningProject1920"
     },
     {
         id : 4,
         name : "SRT Editor",
         description : "Simple srt file editing tool created to aid me in my manual modification (and occasional creation) of subtitle files.",
-        imgPath : "./Images/Projects/placeholder.jpg",
+        imgPath : "./Images/Projects/SrtEditor.png",
         technologies : [
             "C++"
         ],
@@ -43,22 +46,25 @@ export const ProjectData =
     },
     {
         id : 5,
-        name : "PROG 2019/2020",
-        description : "Repository for MIEIC's PROG class of the year 2021/2022. Contains the projects my group developed for this class: games based on oware and scrabble.",
-        imgPath : "./Images/Projects/placeholder.jpg",
+        name : "Library Tracker",
+        description : "Command line utility I made to track my card collection. Allows adding, removing, editing, etc.. Additionally, this program fetches prices for Yu-Gi-Oh cards in the collection from cardmarket. WIP",
+        imgPath : "./Images/Projects/LibraryTracker.jpg",
         technologies : [
-            "C++"
+            "Java",
+            "Gradle",
+            "JSoup"
         ],
-        url : "https://github.com/Retchut/Prog-2019-2020"
+        url : "https://github.com/Retchut/LibraryTracker"
     },
     {
         id : 6,
-        name : "MNUM 2020/2021",
-        description : "Repository for MIEIC's MNUM class of the year 2021/2022. Contains solutions for a few exams I solved while studying, as well as my group's project for this class.",
+        name : "NCGM Learning Project",
+        description : "My learning project developed for NCGM. A simple text based adventure about escaping a locked room.",
+        imgPath : "./Images/Projects/LearningProject.png",
         technologies : [
-            "Python"
+            "Unity",
+            "C#"
         ],
-        imgPath : "./Images/Projects/placeholder.jpg",
-        url : "https://github.com/Retchut/MNUM-2020-2021"
+        url : "https://github.com/Retchut/LearningProject1920"
     }
 ];
