fe44241189d0f5be3312f1dac9d3d63b585654fb
retchut <retchut@protonmail.ch>
2022-05-10 02:41:03 +0100

    feat: add simple home page

---
+++ src/Assets/Data/HomeData.js
@@ -0,0 +1,8 @@
+export const HomeData = {
+    welcome : [
+        "Hello! My name is Mário.",
+        "Welcome to my humble abode!",
+        "This is very much really rough around the edges, but I'll be updating it frequently with more goodies, so don't hesitate to come back later to check up on all that has changed around here!",
+        "Hope to see you again soon     (＠＾◡＾)ノ～"
+    ]
+}
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,20 @@
+import { HomeData } from '../../Assets/Data/HomeData.js'
+
 function Home(){
     return (
         <>
             <h1>Home sweet home</h1>
             <hr></hr>
-            <p>Home description</p>
+            <div className="row">
+                <div className="col pb-4">
+                    <div className="background-box rounded mr-3 p-4">
+                        {HomeData.welcome.map((paragraph, index) => {
+                                return (<p key={"welcome-" + index}>{paragraph}</p>)
+                            }
+                        )}
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
