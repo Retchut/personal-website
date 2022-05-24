74eef4eb9157e6e88151ab8564d056fc4e4cd719
retchut <retchut@protonmail.ch>
2022-05-24 12:05:19 +0100

    chore: update projects

--- src/Assets/Data/ProjectData.js
+++ src/Assets/Data/ProjectData.js
@@ -5,13 +5,9 @@ export const ProjectData =
         name : "This website",
         description : "Repository holding this website's code. Pages were made to be independent from the data displayed on them. WIP",
         technologies : [
-            "React",
-            "Bootstrap 5",
-            "HTML",
-            "CSS",
-            "JavaScript"
+            "React",    "Bootstrap 5",  "HTML", "CSS",  "JavaScript"
         ],
-        imgPath : "./Images/Projects/ReactLogo.png",
+        imgPath : "./Images/Projects/react-logo.png",
         url : "https://github.com/Retchut/personal-website"
     },
     {
@@ -21,46 +17,74 @@ export const ProjectData =
         technologies : [
             "bash"
         ],
-        imgPath : "./Images/Projects/ArchQuickSetup.jpg",
+        imgPath : "./Images/Projects/arch-quick-setup.jpg",
         url : "https://github.com/Retchut/arch-quick-setup"
     },
     {
         id : 3,
+        name : "NCGM's Website",
+        description : "NCGM's website frontend and backend",
+        technologies : [
+            "React",    "Bootstrap5",   "Express",  "Node.js",  "MongoDB",  "Javascript",   "CSS",  "HTML"
+        ],
+        imgPath :"./Images/Projects/ncgm-website.png",
+        url : "https://github.com/ncgmfeup/website-mern"
+    },
+    {
+        id : 4,
+        name : "NCGM VR Workshop",
+        description : "End product of NCGM's VR workshop I attended.",
+        technologies : [
+            "WebXR", "Javascript",   "CSS",  "HTML"
+        ],
+        imgPath :"./Images/Projects/ncgm-vr-workshop.png",
+        url : "https://github.com/Retchut/ncgm-workshop-vr"
+    },
+    {
+        id : 5,
         name : "LCOM 2020/2021",
         description : "Repository for MIEIC's LCOM class of the year 2021/2022. Contains the lab assignments and my final project project for the class,a clone of the classic Space Invaders.",
         technologies : [
             "C"
         ],
-        imgPath : "./Images/Projects/LCOM-2020-2021.png",
+        imgPath : "./Images/Projects/lcom-2020-2021.png",
         url : "https://github.com/Retchut/LCOM-2020-2021"
     },
     {
-        id : 4,
+        id : 6,
         name : "SRT Editor",
         description : "Simple srt file editing tool created to aid me in my manual modification (and occasional creation) of subtitle files.",
-        imgPath : "./Images/Projects/SrtEditor.png",
+        imgPath : "./Images/Projects/srt-editor.png",
         technologies : [
             "C++"
         ],
         url : "https://github.com/Retchut/srt-editor"
     },
     {
-        id : 5,
+        id : 7,
         name : "Library Tracker",
         description : "Command line utility I made to track my card collection. Allows adding, removing, editing, etc.. Additionally, this program fetches prices for Yu-Gi-Oh cards in the collection from cardmarket. WIP",
-        imgPath : "./Images/Projects/LibraryTracker.jpg",
+        imgPath : "./Images/Projects/library-tracker.jpg",
         technologies : [
-            "Java",
-            "Gradle",
-            "JSoup"
+            "Java", "Gradle",   "JSoup"
         ],
         url : "https://github.com/Retchut/LibraryTracker"
     },
     {
-        id : 6,
+        id : 8,
+        name : "lbaw2115 Project",
+        description : "Project developed by my group for the Laboratory of Databases and Web Applications (LBAW).",
+        imgPath : "./Images/Projects/lbaw2115.png",
+        technologies : [
+            "PHP",  "PostgreSQL",   "Laravel",  "Bootstrap",    "JavaScript",   "Docker"
+        ],
+        url : "https://github.com/Retchut/lbaw2115"
+    },
+    {
+        id : 9,
         name : "NCGM Learning Project",
         description : "My learning project developed for NCGM. A simple text based adventure about escaping a locked room.",
-        imgPath : "./Images/Projects/LearningProject.png",
+        imgPath : "./Images/Projects/learning-project.png",
         technologies : [
             "Unity",
             "C#"
@@ -68,18 +92,43 @@ export const ProjectData =
         url : "https://github.com/Retchut/LearningProject1920"
     },
     {
-        id : 7,
-        name : "lbaw2115 Project",
-        description : "Project developed by my group for the Laboratory of Databases and Web Applications (LBAW).",
-        imgPath : "./Images/Projects/LearningProject.png",
+        id : 10,
+        name : "SOPE-2020-2021",
+        description : "Projects developed for MIEIC's 2019/2020 SOPE class (2nd year, 2nd semester)",
+        imgPath : "./Images/Projects/uni-project-placeholder.png",
         technologies : [
-            "PHP",
-            "PostgreSQL",
-            "Laravel",
-            "Bootstrap",
-            "JavaScript",
-            "Docker"
+            "C"
         ],
-        url : "https://github.com/Retchut/lbaw2115"
+        url : "https://github.com/Retchut/SOPE-2020-2021"
+    },
+    {
+        id : 11,
+        name : "CAL-2020-2021-PROJ",
+        description : "Projects developed for MIEIC's 2019/2020 PROG class (1st year, 2nd semester) ",
+        imgPath : "./Images/Projects/uni-project-placeholder.png",
+        technologies : [
+            "C++"
+        ],
+        url : "https://github.com/Retchut/CAL-2020-2021-PROJ"
+    },
+    {
+        id : 12,
+        name : "rcom-data-link-protocol",
+        description : "Projects developed for MIEIC's 2019/2020 PROG class (1st year, 2nd semester) ",
+        imgPath : "./Images/Projects/uni-project-placeholder.png",
+        technologies : [
+            "C"
+        ],
+        url : "https://github.com/Retchut/rcom-data-link-protocol"
+    },
+    {
+        id : 13,
+        name : "PROG-2019-2020",
+        description : "Projects developed for MIEIC's 2019/2020 PROG class (1st year, 2nd semester).",
+        imgPath : "./Images/Projects/uni-project-placeholder.png",
+        technologies : [
+            "C++"
+        ],
+        url : "https://github.com/Retchut/Prog-2019-2020"
     }
 ];
