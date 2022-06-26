c0d742e3caefe9f4bbdb489c4cc29f8157cbaeb9
retchut <retchut@protonmail.ch>
2022-06-26 21:13:36 +0100

    chore: update home and about texts

--- src/Assets/Data/AboutData.js
+++ src/Assets/Data/AboutData.js
@@ -1,7 +1,8 @@
 export const AboutData = {
     aboutMe : [
-        "Hello! My name is Mário.",
-        "I'm an aspiring Software Developer from Portugal. I'm deeply interested in Game Design, Game Development and Web Dev.",
-        "When I'm not studying or working in the aforementioned areas, you may find me reparing and/or tinkering with electronics, playing video, board and card games, or consuming all kinds of media."
+        "Hello! My name is Mário, an aspiring Software Developer from Portugal. I'm deeply interested in Game Design, Game Development and Web Dev.",
+        "At the moment, I'm attending the 3rd year of an Integrated Master's Degree in Informatics and Computer Engineering at FEUP.",
+        "When I'm not studying or working in the aforementioned areas, you may find me reparing and/or tinkering with electronics, playing video, board and card games, or consuming all kinds of media.",
+        "I'm currently the President of the Nucleus of Computer Graphics and Multimedia of FEUP, a youth association focused on Computer Graphics, Human-Computer Interaction and Game Development. We host a multitude of events, but our biggest one is the Game Dev Meet @Porto, a monthly gathering of game dev enthusiasts, with the intent of making it easier for aspiring developers to connect with other like-minded individuals, obtain feedback about their projects, and network with both amateurs and professionals of this area alike.",
     ]
 };
